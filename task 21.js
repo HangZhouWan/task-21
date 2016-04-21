e0922d6a85b4b516e828b097e58320a52fa94136
HangZhouWan <yydcqc@163.com>
2016-04-21 16:47:04 +0800

    Update task 21.js

--- task 21.js
+++ task 21.js
@@ -3,10 +3,9 @@
 function getId(Id){
 	return document.getElementById(Id);
 }
-
+var tag = getId("tag")
 var	tagBtn = getId("tagBtn");
 var	tagContainer = document.getElementById("tagContainer");
-var	inters  = getId("inters").value.trim();
 var	intersBtn = getId("intersBtn");
 var	intersContainer = getId("intersContainer");
 var	tagData = [];
@@ -25,30 +24,68 @@ function render(arr,elem,flag){
 	arr.forEach(function(e){inner+="<span>"+e+"</span>"});
 	elem.innerHTML = inner;
 	if(flag){	
-	delClick(arr,elem);
+	delClick(arr,elem,flag);
 	};
 }
 
-function delClick(arr,elem){
+function delClick(arr,elem,flag){
 	var child = elem.childNodes;
-	for(var i in child){
+	for(var i = 0;i<child.length;i++){
 		(function(index){
+			var inner = child[index].innerHTML;
 			child[index].onclick = function(){
 				arr.splice(index,1);
-				render(arr,elem);
+				render(arr,elem,flag);
+			}
+			child[index].onmouseover = function(){
+				child[index].innerHTML = "删除"+inner+"?";
+			}
+			child[index].onmouseout = function(){
+				child[index].innerHTML = inner;
 			}
 		}(i));
 	}
 }
 
+function autoAdd(inputText,arr,Container,e,flag){
+	var autoAddRules = /(\s|,|，|、)/g;
+	if(autoAddRules.test(inputText)||e.keyCode === 13){
+		inputText = inputText.replace(autoAddRules,"");
+		if(inputText == " "){
+			alert("请输入内容！");
+			document.getElementById("tag").value = "";
+			return false;
+		}
+		var mark = true;
+		var len = arr.length;
+		if(arr.indexOf(inputText)>=0){
+			mark = false;
+		}
+		if(arr.length>=10&&mark){
+			arr.shift();
+			arr.push(inputText);
+		}
+		else if(mark){
+			arr.push(inputText);
+		}
+		render(arr,Container,flag);
+		document.getElementById("tag").value = "";
+	}
+
+}
+
 
 function init(){
-	tagBtn.onclick = function(){
-		var tag = document.getElementById("tag").value.trim();
-		var newData = toArr(tag);
-		tagData = tagData.concat(newData);
-		render(tagData,tagContainer,true);
-	}
+	tag.addEventListener("keyup" ,function(e){
+		var tagText = document.getElementById("tag").value.trim();
+		autoAdd(tagText,tagData,tagContainer,e,true);
+	});
+    intersBtn.onclick = function(){
+    	var	inters  = getId("inters").value.trim();
+    	getId("inters").value = "";
+    	intersData = intersData.concat(toArr(inters));
+    	render(intersData,intersContainer,true);
+    }
 }
 		
 init();	
